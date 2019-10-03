b89ed5c30220dba73d6f91395f9c30f95d391423
yannvgn <hi@yannvgn.io>
2019-10-03 10:13:37 +0200

    add romanization also, fix preprocessing steps order

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -3,19 +3,22 @@ import pytest
 from laserembeddings import Laser
 from laserembeddings.preprocessing import Tokenizer, BPE
 
+from laserembeddings.utils import sre_performance_patch
+
 
 def test_tokenizer():
-    assert Tokenizer('en').tokenize("Let's do it!") == "let 's do it !"
+    with sre_performance_patch():
+        assert Tokenizer('en').tokenize("Let's do it!") == "let 's do it !"
 
-    with pytest.raises(NotImplementedError):
-        Tokenizer(romanize=True)
+        assert Tokenizer(
+            'en', descape=True).tokenize("Let's do it &amp; pass that test!"
+                                         ) == "let 's do it & pass that test !"
 
-    assert Tokenizer(
-        'en', descape=True).tokenize("Let's do it &amp; pass that test!"
-                                     ) == "let 's do it & pass that test !"
+        with pytest.raises(AssertionError):
+            Tokenizer(lower_case=False)
 
-    with pytest.raises(AssertionError):
-        Tokenizer(lower_case=False)
+        assert not Tokenizer('en').romanize
+        assert Tokenizer('el').romanize
 
 
 def test_bpe():
