eb51a26b7609910025e25847fc7bd7646529fedd
Chirag Jain <jain.chirag925@gmail.com>
2019-10-10 19:43:38 +0530

    Use fastBPE from pypi instead of subword nmt

--- laserembeddings/__main__.py
+++ laserembeddings/__main__.py
@@ -95,7 +95,7 @@ def main():
                 "❌  Looks like you're not running laserembeddings from its source code"
             )
             print(
-                "     → please checkout https://github.com/yannvgn/laserembedings.git"
+                "     → please checkout https://github.com/yannvgn/laserembeddings.git"
             )
             print(
                 '       then run "python -m laserembeddings download-test-data" from the root of the repository'
--- laserembeddings/utils.py
+++ laserembeddings/utils.py
@@ -1,35 +1,4 @@
-from typing import TextIO
-
-__all__ = ['BPECodesAdapter', 'sre_performance_patch']
-
-
-class BPECodesAdapter:
-    """
-    A file object kind-of wrapper converting fastBPE codes to subword_nmt BPE codes.
-
-    Args:
-        bpe_codes_f (TextIO): the text-mode file object of fastBPE codes
-    """
-
-    def __init__(self, bpe_codes_f: TextIO):
-        self.bpe_codes_f = bpe_codes_f
-
-    def seek(self, offset: int, whence: int = 0) -> int:
-        return self.bpe_codes_f.seek(offset, whence)
-
-    def readline(self, limit: int = -1) -> str:
-        return self._adapt_line(self.bpe_codes_f.readline(limit))
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return self._adapt_line(next(self.bpe_codes_f))
-
-    @staticmethod
-    def _adapt_line(line: str) -> str:
-        parts = line.strip('\r\n ').split(' ')
-        return ' '.join(parts[:-1]) + '\n' if len(parts) == 3 else line
+__all__ = ['sre_performance_patch']
 
 
 class sre_performance_patch:
@@ -43,7 +12,7 @@ class sre_performance_patch:
         self.original_sre_parse_uniq = None
 
     def __enter__(self):
-        #pylint: disable=import-outside-toplevel
+        # pylint: disable=import-outside-toplevel
         import sys
 
         if self.original_sre_parse_uniq is None and (
@@ -52,7 +21,7 @@ class sre_performance_patch:
             try:
                 import sre_parse
                 self.sre_parse = sre_parse
-                #pylint: disable=protected-access
+                # pylint: disable=protected-access
                 self.original_sre_parse_uniq = sre_parse._uniq
                 sre_parse._uniq = lambda x: list(dict.fromkeys(x))
             except (ImportError, AttributeError):
@@ -61,6 +30,6 @@ class sre_performance_patch:
 
     def __exit__(self, type_, value, traceback):
         if self.sre_parse and self.original_sre_parse_uniq:
-            #pylint: disable=protected-access
+            # pylint: disable=protected-access
             self.sre_parse._uniq = self.original_sre_parse_uniq
             self.original_sre_parse_uniq = None
