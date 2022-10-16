200d4fe6c779505ccbd9b477b79c3ef5a9fbbf1f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-16 03:21:17 +0000

    Adding tarfile member sanitization to extractall()

--- laserembeddings/__main__.py
+++ laserembeddings/__main__.py
@@ -48,7 +48,26 @@ def extract_tar(tar, output_dir):
     print(f'{CONSOLE_WAIT}   Extracting archive...', end='')
     sys.stdout.flush()
     with tarfile.open(tar) as t:
-        t.extractall(output_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(t, output_dir)
     print(f'\r{CONSOLE_DONE}   Extracted archive    ')
 
 
